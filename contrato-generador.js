83d1626f8620897a04b4d7ffb98ae87ea762fefb
Jorge200118 <jorgefelixa1810@gmail.com>
2026-07-22 17:06:30 -0700

    feat(contrato): CDN docxtemplater + esqueleto de generador

---
+++ contrato-generador.js
@@ -0,0 +1,7 @@
+// contrato-generador.js
+// Generación automática del Contrato Individual de Trabajo al dar de alta un empleado.
+// Se rellena en el navegador con docxtemplater + PizZip a partir de plantilla-contrato.docx.
+(function () {
+  'use strict';
+  console.log('[contrato-generador] cargado');
+})();
